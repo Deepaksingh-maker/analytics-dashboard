c9c898c0a12463c9d779cd3c25b89dad3851a806
Deepaksingh-maker <bagheldeepak5400@gmail.com>
2026-01-03 16:31:50 +0530

    fix base path for github pages

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: "/analytics-dashboard/",
-  server: {
-    port: 5173,
-    open: true
-  }
+  base: '/analytics-dashboard/',
 })
